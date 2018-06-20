174041e10f87683ed04bcb61e40a5dc0398a2b3c
mtraina <matteo.traina.mail@gmail.com>
2018-06-20 21:36:33 +0200

    test added

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,3 +2,15 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 // capitalized because it is a constractor function
 const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts)
+        })
+})
+
+describe('Inbox', () => {
+    it('deploys a contract', () => {})
+})
