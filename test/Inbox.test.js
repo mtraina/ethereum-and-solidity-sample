ecbdf66e48e0387be2ba0514565d30db0ab12a5c
mtraina <matteo.traina.mail@gmail.com>
2018-06-21 20:55:35 +0200

    deploy contract I

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,11 +3,17 @@ const ganache = require('ganache-cli');
 // capitalized because it is a constractor function
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!']})
+        .send({ from: accounts[0], gas: '1000000'})
 })
 
 describe('Inbox', () => {
