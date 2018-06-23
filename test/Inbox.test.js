0b4bb46d6451141f36e89aba5c970e30a1b7a62f
mtraina <matteo.traina.mail@gmail.com>
2018-06-23 14:42:33 +0200

    check initial mesage test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,7 +2,11 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 // capitalized because it is a constractor function
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+// UPDATE THESE TWO LINES RIGHT HERE!!!!! <-----------------
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -14,10 +18,17 @@ beforeEach(async () => {
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hi there!']})
         .send({ from: accounts[0], gas: '1000000'})
+
+    // ADD THIS ONE LINE RIGHT HERE!!!!! <---------------------
+    inbox.setProvider(provider);
 })
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        assert.ok(inbox.options.address)
-    })
-})
+      assert.ok(inbox.options.address);
+    });
+    it('has a default message', async () => {
+      const message = await inbox.methods.message().call();
+      assert.equal(message, 'Hi there!');
+    });
+  });
