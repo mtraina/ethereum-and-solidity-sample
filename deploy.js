c39d02a6c0be730f18189659c533eb09775bc1d6
mtraina <matteo.traina.mail@gmail.com>
2018-06-24 15:38:41 +0200

    refactoring

--- deploy.js
+++ deploy.js
@@ -1,14 +1,12 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
 const Web3 = require('web3')
 const { interface, bytecode } = require('./compile')
-const { mnemonic } = require('./conf')
+const { mnemonic, rinkeby } = require('./conf')
 
 console.log('mnemonic is: ', mnemonic)
 
 const provider = new HDWalletProvider(
-    mnemonic,
-    'https://rinkeby.infura.io/fbeXCMxaXyB0RUt5C9YT'
-
+    mnemonic, rinkeby
 )
 const web3 = new Web3(provider)
 
